016922e26cdcfa2d156e85a02928b62bb9894026
whoisebvka <vebuzor@gmail.com>
2024-01-02 12:39:43 -0400

    fixed uplaodthing error

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/imageForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/imageForm.tsx
@@ -15,12 +15,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Pencil } from 'lucide-react';
+import { ImageIcon, Pencil, PlusCircle } from 'lucide-react';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { Textarea } from '@/components/ui/textarea';
+import { Course } from '@prisma/client';
+import Image from 'next/image';
+import FileUpload from '@/components/fileUpload';
 
 
 
@@ -28,15 +31,13 @@ import { Textarea } from '@/components/ui/textarea';
 type Props = {}
 
 interface ImageFormProps {
-    initialData: {
-        description: string
-    };
+    initialData: Course;
     courseId: string
 }
 
 const formSchema = z.object({
-    description: z.string().min(2, {
-      message: "Description is required",
+    imageUrl: z.string().min(2, {
+      message: "Image is required",
     }),
   })
   
@@ -49,7 +50,9 @@ export default function ImageForm({initialData, courseId}: ImageFormProps) {
     const toggleEdit = () => setIsEditing((current) => !current)
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-        defaultValues: initialData,
+        defaultValues: {
+            imageUrl: initialData?.imageUrl || ""
+        },
       });
 
     const { isSubmitting, isValid } = form.formState
@@ -68,64 +71,56 @@ export default function ImageForm({initialData, courseId}: ImageFormProps) {
   return (
     <div className='mt-6 border bg-slate-100 rounded-md p-4'>
         <div className="font-medium flex items-center justify-between">
-            Course Description
+            Course Image
             <Button onClick={toggleEdit} variant="ghost">
-                {isEditing ? (
+                {isEditing && (
                     <>Cancel</>
-                ): (
+                )} 
+                {!isEditing && !initialData.imageUrl && (
+                    <>
+                        <PlusCircle className='h-4 w-4 mr-2' />
+                        Add an Image
+                    </>
+                )}
+
+                {!isEditing && initialData.imageUrl && (
                     <>
                         <Pencil className='h-4 w-4 mr-2' />
-                        Edit Description
+                        Edit Image
                     </>
                 )}
             </Button>
         </div>
         {!isEditing && (
-            <p className={cn(
-                "text-sm mt-2",
-                !initialData.description && "text-slate-500 ital;ic"
-            )}>
-                {initialData.description || "No descrption"}
-            </p>
+            !initialData.imageUrl ? (
+                <div className='flex items-center justify-center h-60 bg-slate-200 rounded-md'>
+                    <ImageIcon className='h-10 w-10 text-slate-500' />
+                </div>
+            ) : (
+                <div className='relative aspect-video mt-2'>
+                   <Image
+                        alt="upload"
+                        fill
+                        className='object-cover rounded-md'
+                        src={initialData.imageUrl}
+                     />
+                </div>
+            )
         )}
         {isEditing && (
-            <Form {...form}>
-                <form 
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className='space-y-8 mt-4'
-                >
-                    <FormField 
-                        control={form.control}
-                        name='description'
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Course Description
-                                </FormLabel>
-                                <FormControl>
-                                    <Textarea 
-                                        disabled={isSubmitting}
-                                        placeholder="e.g. `This course is about learning to work in an organizational...`"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    Describe what the course is meant to teach
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <div className="flex itesm-center gap-x-2">
-                        <Button
-                            disabled={!isValid || isSubmitting}
-                            type='submit'
-                        >
-                            Save
-                        </Button>
-                    </div>
-                </form>
-            </Form>
+            <div>
+                <FileUpload 
+                    endpoint='courseImage'
+                    onChange={(url) => {
+                        if(url) {
+                            onSubmit({ imageUrl: url })
+                        }
+                    }}
+                />
+                <div className="text-xs text-muted-foreground mt-4">
+                    16:9 aspect ratio recommended
+                </div>
+            </div>
         )}
     </div>
   )
