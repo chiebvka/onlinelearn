843ee02523d34d6b2ada7709524d00f28ea2f87a
whoisebvka <vebuzor@gmail.com>
2024-01-07 15:33:26 -0400

    individual course details page

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         alpha: "bg-emerald-400 text-primary-foreground",
+        success: "bg-emerald-600 text-white hover:bg-emerald-600/80"
       },
       size: {
         default: "h-10 px-4 py-2",
